f3b15cff77dd7386a283343e1955e1ae0d0052dc
pondsuyeon <pondsuyeon@gmail.com>
2021-01-29 00:02:56 +0900

    1026 treasure

---
+++ 1026/1026.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            B[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+            sum += A[i] * B[i];
+        System.out.println(sum);
+    }
+}
