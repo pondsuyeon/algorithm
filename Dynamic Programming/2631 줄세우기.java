87f65f1595d2ec13475edd70ca857069b44aa32c
pondsuyeon <pondsuyeon@gmail.com>
2021-06-23 20:44:29 +0900

    2631 줄세우기

---
+++ Dynamic Programming/2631 줄세우기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(N - max);
+    }
+}
