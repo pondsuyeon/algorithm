0833916d67ee0d360e50487ae9574d86c13f3014
pondsuyeon <pondsuyeon@gmail.com>
2022-03-21 22:28:31 +0900

    BOJ 10926 ??!

---
+++ etc/Main_BOJ_10926_Joonas.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main_BOJ_10926_Joonas {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
+}
