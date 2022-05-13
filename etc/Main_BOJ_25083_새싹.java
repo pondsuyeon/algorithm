54571940c17570146e5a09b87fce97afc31bfa12
pondsuyeon <pondsuyeon@gmail.com>
2022-05-13 20:59:50 +0900

    BOJ 25083 새싹

---
+++ etc/Main_BOJ_25083_새싹.java
@@ -0,0 +1,12 @@
+package Y22.M05;
+
+public class Main_BOJ_25083_새싹 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
